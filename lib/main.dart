9cf7cd5f062702ee071c03a5529d5ebd62f8774a
hmb <mstfhll353@gmail.com>
2023-04-03 01:07:50 +0300

    DeepPurple theme

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const LoginScreen(),
     );
